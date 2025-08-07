ef2807564bd3fbc424f69a730ec07a575192d08d
ralgcorp <ralgcorp@gmail.com>
2025-08-07 06:45:17 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/products-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
