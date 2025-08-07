6d2957b53d5339ac47aa0bb5eb184fa1fe5ab009
ralgcorp <ralgcorp@gmail.com>
2025-08-07 17:54:38 -0300

    chore: add shipping address table

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -13,6 +13,7 @@ import {
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
@@ -23,7 +24,7 @@ export const Header = () => {
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-3">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
@@ -78,6 +79,7 @@ export const Header = () => {
             </div>
           </SheetContent>
         </Sheet>
+        <Cart />
       </div>
     </header>
   );
