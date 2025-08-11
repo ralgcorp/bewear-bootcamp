5c5cc87ee8bbbc23fe396f8d276ba242a5c2b0d0
ralgcorp <ralgcorp@gmail.com>
2025-08-11 16:10:13 -0300

    feat: add checkout success page

--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -1,84 +1,47 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+"use client";
 
-import Footer from "@/components/common/footer";
-import { Header } from "@/components/common/header";
+import { loadStripe } from "@stripe/stripe-js";
+import { Loader2 } from "lucide-react";
+
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { db } from "@/db";
-import { auth } from "@/lib/auth";
+import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 
-import CartSummary from "../components/cart-summary";
-import { formatAddress } from "../helpers/address";
-import FinishOrderButton from "./components/finish-order-button";
-
-const ConfirmationPage = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (!session?.user.id) {
-    redirect("/");
-  }
-  const cart = await db.query.cartTable.findFirst({
-    where: (cart, { eq }) => eq(cart.userId, session.user.id),
-    with: {
-      shippingAddress: true,
-      items: {
-        with: {
-          productVariant: {
-            with: {
-              product: true,
-            },
-          },
-        },
-      },
-    },
-  });
-  if (!cart || cart?.items.length === 0) {
-    redirect("/");
-  }
-  const cartTotalInCents = cart.items.reduce(
-    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
-    0,
-  );
-  if (!cart.shippingAddress) {
-    redirect("/cart/identification");
-  }
+const FinishOrderButton = () => {
+  const finishOrderMutation = useFinishOrder();
+  const handleFinishOrder = async () => {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key is not set");
+    }
+    const { orderId } = await finishOrderMutation.mutateAsync();
+    const checkoutSession = await createCheckoutSession({
+      orderId,
+    });
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+    await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+  };
   return (
-    <div>
-      <Header />
-      <div className="space-y-4 px-5">
-        <Card>
-          <CardHeader>
-            <CardTitle>Identificação</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-6">
-            <Card>
-              <CardContent>
-                <p className="text-sm">{formatAddress(cart.shippingAddress)}</p>
-              </CardContent>
-            </Card>
-            <FinishOrderButton />
-          </CardContent>
-        </Card>
-        <CartSummary
-          subtotalInCents={cartTotalInCents}
-          totalInCents={cartTotalInCents}
-          products={cart.items.map((item) => ({
-            id: item.productVariant.id,
-            name: item.productVariant.product.name,
-            variantName: item.productVariant.name,
-            quantity: item.quantity,
-            priceInCents: item.productVariant.priceInCents,
-            imageUrl: item.productVariant.imageUrl,
-          }))}
-        />
-      </div>
-      <div className="mt-12">
-        <Footer />
-      </div>
-    </div>
+    <>
+      <Button
+        className="w-full rounded-full"
+        size="lg"
+        onClick={handleFinishOrder}
+        disabled={finishOrderMutation.isPending}
+      >
+        {finishOrderMutation.isPending && (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        )}
+        Finalizar compra
+      </Button>
+    </>
   );
 };
 
-export default ConfirmationPage;
+export default FinishOrderButton;
